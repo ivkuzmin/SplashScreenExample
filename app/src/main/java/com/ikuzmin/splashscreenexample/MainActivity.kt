4fea4a274e40f245a794f3f14e6bc7c3b580148a
Ilia Kuzmin <i.kyzmin.v@gmail.com>
2021-07-20 19:17:03 +0300

    final commit

--- app/src/main/java/com/ikuzmin/splashscreenexample/MainActivity.kt
+++ app/src/main/java/com/ikuzmin/splashscreenexample/MainActivity.kt
@@ -1,35 +1,41 @@
 package com.ikuzmin.splashscreenexample
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity(),com.ikuzmin.splashscreenexample.View {
+class MainActivity : AppCompatActivity(), com.ikuzmin.splashscreenexample.View {
   private var isReady = false
   private val presenter = SplashPresenter()
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    installSplashScreen()
+    val splashScreen = installSplashScreen()
     setContentView(R.layout.activity_main)
-    val content: View = findViewById(android.R.id.content)
+    splashScreen.setKeepVisibleCondition {
+      return@setKeepVisibleCondition !isReady
+    }
 
-    content.viewTreeObserver.addOnPreDrawListener(
-      object : ViewTreeObserver.OnPreDrawListener {
-        override fun onPreDraw(): Boolean {
-          // Check if the initial data is ready.
-          return if (isReady) {
-            // The content is ready; start drawing.
-            content.viewTreeObserver.removeOnPreDrawListener(this)
-            true
-          } else {
-            // The content is not ready; suspend.
-            false
-          }
-        }
-      }
-    )
+    splashScreen.setOnExitAnimationListener { splashScreenView ->
+      val slideUp = ObjectAnimator.ofFloat(
+        splashScreenView.view,
+        View.TRANSLATION_Y,
+        0f,
+        -splashScreenView.view.height.toFloat()
+      )
+      slideUp.interpolator = AnticipateInterpolator()
+      slideUp.duration = 200L
+
+      // Call SplashScreenView.remove at the end of your custom animation.
+      slideUp.doOnEnd { splashScreenView.remove() }
+
+      // Run your animation.
+      slideUp.start()
+    }
     presenter.attach(this)
     presenter.startJob()
   }
